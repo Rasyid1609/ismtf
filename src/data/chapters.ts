f61fa5207c2c204216100152d45078d2c06aea6d
Rasyid1609 <rasyid.mahardian@gmail.com>
2026-05-02 19:42:10 +0700

    changes to webp and add some file

--- src/data/chapters.ts
+++ src/data/chapters.ts
@@ -5,34 +5,34 @@ export const chapters: Chapter[] = [
     id: "1",
     title: "Chapter 201",
     images: [
-      "/images/ch201/01.png",
-      "/images/ch201/02.png",
-      "/images/ch201/03.png",
-      "/images/ch201/04.png",
-      "/images/ch201/05.png",
-      "/images/ch201/06.png",
-      "/images/ch201/07.png",
-      "/images/ch201/08.png",
-      "/images/ch201/09.png",
-      "/images/ch201/10.png",
-      "/images/ch201/11.png"
+      "/images/ch201/01.webp",
+      "/images/ch201/02.webp",
+      "/images/ch201/03.webp",
+      "/images/ch201/04.webp",
+      "/images/ch201/05.webp",
+      "/images/ch201/06.webp",
+      "/images/ch201/07.webp",
+      "/images/ch201/08.webp",
+      "/images/ch201/09.webp",
+      "/images/ch201/10.webp",
+      "/images/ch201/11.webp"
     ]
   },
   {
     id: "2",
     title: "Chapter 202",
     images: [
-      "/images/ch202/01.png",
-      "/images/ch202/02.png",
-      "/images/ch202/03.png",
-      "/images/ch202/04.png",
-      "/images/ch202/05.png",
-      "/images/ch202/06.png",
-      "/images/ch202/07.png",
-      "/images/ch202/08.png",
-      "/images/ch202/09.png",
-      "/images/ch202/10.png",
-      "/images/ch202/11.png"
+      "/images/ch202/01.webp",
+      "/images/ch202/02.webp",
+      "/images/ch202/03.webp",
+      "/images/ch202/04.webp",
+      "/images/ch202/05.webp",
+      "/images/ch202/06.webp",
+      "/images/ch202/07.webp",
+      "/images/ch202/08.webp",
+      "/images/ch202/09.webp",
+      "/images/ch202/10.webp",
+      "/images/ch202/11.webp"
     ]
   }
 ];
