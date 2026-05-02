c5dd2b1202d9f5ea7e4f542731fd5f4ea163dc5f
Rasyid1609 <rasyid.mahardian@gmail.com>
2026-05-02 20:28:44 +0700

    add data

--- src/data/chapters.ts
+++ src/data/chapters.ts
@@ -3,6 +3,192 @@ import { Chapter } from "@/types/chapter";
 export const chapters: Chapter[] = [
   {
     id: "1",
+    title: "Chapter 192",
+    images: [
+      "/images/ch192/01.webp",
+      "/images/ch192/02.webp",
+      "/images/ch192/03.webp",
+      "/images/ch192/04.webp",
+      "/images/ch192/05.webp",
+      "/images/ch192/06.webp",
+      "/images/ch192/07.webp",
+      "/images/ch192/08.webp",
+      "/images/ch192/09.webp",
+      "/images/ch192/10.webp",
+      "/images/ch192/11.webp",
+      "/images/ch192/12.webp",
+      "/images/ch192/13.webp",
+      "/images/ch192/14.webp",
+      "/images/ch192/15.webp",
+
+    ]
+  },
+  {
+    id: "2",
+    title: "Chapter 193",
+    images: [
+      "/images/ch193/01.webp",
+      "/images/ch193/02.webp",
+      "/images/ch193/03.webp",
+      "/images/ch193/04.webp",
+      "/images/ch193/05.webp",
+      "/images/ch193/06.webp",
+      "/images/ch193/07.webp",
+      "/images/ch193/08.webp",
+      "/images/ch193/09.webp",
+      "/images/ch193/10.webp",
+      "/images/ch193/11.webp"
+    ]
+  },
+  {
+    id: "3",
+    title: "Chapter 194",
+    images: [
+      "/images/ch194/01.webp",
+      "/images/ch194/02.webp",
+      "/images/ch194/03.webp",
+      "/images/ch194/04.webp",
+      "/images/ch194/05.webp",
+      "/images/ch194/06.webp",
+      "/images/ch194/07.webp",
+      "/images/ch194/08.webp",
+      "/images/ch194/09.webp",
+      "/images/ch194/10.webp",
+      "/images/ch194/11.webp",
+      "/images/ch194/12.webp"
+    ]
+  },
+  {
+    id: "4",
+    title: "Chapter 195",
+    images: [
+      "/images/ch195/01.webp",
+      "/images/ch195/02.webp",
+      "/images/ch195/03.webp",
+      "/images/ch195/04.webp",
+      "/images/ch195/05.webp",
+      "/images/ch195/06.webp",
+      "/images/ch195/07.webp",
+      "/images/ch195/08.webp",
+      "/images/ch195/09.webp",
+      "/images/ch195/10.webp",
+      "/images/ch195/11.webp"
+    ]
+  },
+  {
+    id: "5",
+    title: "Chapter 196",
+    images: [
+      "/images/ch196/01.webp",
+      "/images/ch196/02.webp",
+      "/images/ch196/03.webp",
+      "/images/ch196/04.webp",
+      "/images/ch196/05.webp",
+      "/images/ch196/06.webp",
+      "/images/ch196/07.webp",
+      "/images/ch196/08.webp",
+      "/images/ch196/09.webp",
+      "/images/ch196/10.webp",
+      "/images/ch196/11.webp",
+      "/images/ch196/12.webp",
+      "/images/ch196/13.webp",
+      "/images/ch196/14.webp",
+      "/images/ch196/15.webp",
+      "/images/ch196/16.webp"
+    ]
+  },
+  {
+    id: "6",
+    title: "Chapter 197",
+    images: [
+      "/images/ch197/01.webp",
+      "/images/ch197/02.webp",
+      "/images/ch197/03.webp",
+      "/images/ch197/04.webp",
+      "/images/ch197/05.webp",
+      "/images/ch197/06.webp",
+      "/images/ch197/07.webp",
+      "/images/ch197/08.webp",
+      "/images/ch197/09.webp",
+      "/images/ch197/10.webp",
+      "/images/ch197/11.webp",
+      "/images/ch197/12.webp"
+    ]
+  },
+  {
+    id: "7",
+    title: "Chapter 198",
+    images: [
+      "/images/ch198/01.webp",
+      "/images/ch198/02.webp",
+      "/images/ch198/03.webp",
+      "/images/ch198/04.webp",
+      "/images/ch198/05.webp",
+      "/images/ch198/06.webp",
+      "/images/ch198/07.webp",
+      "/images/ch198/08.webp",
+      "/images/ch198/09.webp",
+      "/images/ch198/10.webp",
+      "/images/ch198/11.webp",
+      "/images/ch198/12.webp"
+    ]
+  },
+  {
+    id: "8",
+    title: "Chapter 198",
+    images: [
+      "/images/ch198/01.webp",
+      "/images/ch198/02.webp",
+      "/images/ch198/03.webp",
+      "/images/ch198/04.webp",
+      "/images/ch198/05.webp",
+      "/images/ch198/06.webp",
+      "/images/ch198/07.webp",
+      "/images/ch198/08.webp",
+      "/images/ch198/09.webp",
+      "/images/ch198/10.webp",
+      "/images/ch198/11.webp",
+      "/images/ch198/12.webp"
+    ]
+  },
+  {
+    id: "9",
+    title: "Chapter 199",
+    images: [
+      "/images/ch199/01.webp",
+      "/images/ch199/02.webp",
+      "/images/ch199/03.webp",
+      "/images/ch199/04.webp",
+      "/images/ch199/05.webp",
+      "/images/ch199/06.webp",
+      "/images/ch199/07.webp",
+      "/images/ch199/08.webp",
+      "/images/ch199/09.webp",
+      "/images/ch199/10.webp",
+      "/images/ch199/11.webp"
+    ]
+  },
+  {
+    id: "10",
+    title: "Chapter 200",
+    images: [
+      "/images/ch200/01.webp",
+      "/images/ch200/02.webp",
+      "/images/ch200/03.webp",
+      "/images/ch200/04.webp",
+      "/images/ch200/05.webp",
+      "/images/ch200/06.webp",
+      "/images/ch200/07.webp",
+      "/images/ch200/08.webp",
+      "/images/ch200/09.webp",
+      "/images/ch200/10.webp",
+      "/images/ch200/11.webp",
+      "/images/ch200/12.webp",
+      "/images/ch200/13.webp"
+    ]
+  },
+  {
+    id: "11",
     title: "Chapter 201",
     images: [
       "/images/ch201/01.webp",
@@ -19,7 +205,7 @@ export const chapters: Chapter[] = [
     ]
   },
   {
-    id: "2",
+    id: "12",
     title: "Chapter 202",
     images: [
       "/images/ch202/01.webp",
